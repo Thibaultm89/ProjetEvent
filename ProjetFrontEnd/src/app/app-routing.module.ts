aa9e4a0d880958d72918e98fea4059360017f0ae
Philippe Vancom <philippe.vancom@gmail.com>
2019-06-27 13:55:42 +0200

    new page home

---
+++ ProjetFrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'}, //toujours à mettre en premier
+  {path: 'home', component: HomeComponent},
+  {path: '**', redirectTo: '/home'} //toujours à mettre en dernier
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ ProjetFrontEnd/src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onSendChange(send: string) {
+    console.log(send);
+  }
+}
