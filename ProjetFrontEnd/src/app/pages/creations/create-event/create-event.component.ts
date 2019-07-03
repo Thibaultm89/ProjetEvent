758cab9d095144a1d9550f5a9182d73ad79eee9e
Philippe Vancom <philippe.vancom@gmail.com>
2019-07-03 16:58:15 +0200

    list hour (pas finie)

--- ProjetFrontEnd/src/app/pages/creations/create-event/create-event.component.ts
+++ ProjetFrontEnd/src/app/pages/creations/create-event/create-event.component.ts
@@ -9,6 +9,9 @@ import { DateWithoutTime } from 'src/app/models/date-without-time.model';
 })
 export class CreateEventComponent implements OnInit {
 
+  public hourList = [null, '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+   '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
+
   public event: Event;
 
   public eventForm: FormGroup;
